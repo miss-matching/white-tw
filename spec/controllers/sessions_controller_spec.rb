25a1719e41bfae6fa642384f2af2739d4730fd06
rocky <tak.koroki@gmail.com>
2013-05-06 15:45:04 +0900

    自動生成されたspecたちを一旦黙らせました closes #13

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,19 +1,14 @@
+#coding: utf-8
 require 'spec_helper'
 
 describe SessionsController do
 
   describe "GET 'new'" do
-    it "returns http success" do
-      get 'new'
-      response.should be_success
-    end
+    pending "セッションあるなしで飛ばすところを決めなくちゃ"
   end
 
   describe "GET 'create'" do
-    it "returns http success" do
-      get 'create'
-      response.should be_success
-    end
+    pending "twitter認証をMockしなきゃならんね"
   end
 
 end
