6c3fa34294b2df925993d1dcdbdfe0a6d8ea76e7
rocky <tak.koroki@gmail.com>
2013-05-06 15:17:17 +0900

    NgWordsのCRUDをAccountのshowに実装 closes #5

--- spec/factories/accounts.rb
+++ spec/factories/accounts.rb
@@ -2,9 +2,13 @@
 
 FactoryGirl.define do
   factory :account do
-    twitter_id "MyString"
-    twitter_token "MyString"
-    twitter_secret "MyString"
+    sequence(:twitter_id){ |n| "twitter_id#{n}" }
+    twitter_token "token"
+    twitter_secret "secret"
     last_login "2013-05-04 17:19:35"
+
+    factory :account_with_two_ng_words do 
+      after(:create) { |account| create_list(:ng_word, 2, account: account) }
+    end
   end
 end
---
+++ spec/requests/ng_words_spec.rb
@@ -0,0 +1,13 @@
+#coding: utf-8
+require 'spec_helper'
+
+describe "NgWords" do
+  describe "GET /ng_words" do
+# TODO: 置いておいた
+#    it "works! (now write some real specs)" do
+#      # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
+#      get ng_words_path
+#      response.status.should be(200)
+#    end
+  end
+end
