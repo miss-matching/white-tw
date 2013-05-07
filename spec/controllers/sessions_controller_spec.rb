ef37692b0870441af0dc8bf9bc4a056ac38bdfdb
rocky <tak.koroki@gmail.com>
2013-05-08 01:38:42 +0900

    ログアウト実装

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,13 +2,45 @@
 require 'spec_helper'
 
 describe SessionsController do
+  
+  before :all do
+    @logged_in_account = FactoryGirl.create(:account)
+  end
+
+  let(:logged_in_session) { {:account_id => @logged_in_account.id} }
+  let(:logged_out_session) { {:account_id => nil} }
 
   describe "GET 'new'" do
-    pending "セッションあるなしで飛ばすところを決めなくちゃ"
+    describe "with logged in session" do
+      it "redirects to current account" do
+        get :new,{}, logged_in_session
+        expect( response ).to redirect_to @logged_in_account
+      end
+    end
+    
+    describe "with logged out session" do
+      it "redirects to current account" do
+        get :new,{},logged_out_session
+        expect( response ).to be_success
+      end
+    end
   end
 
   describe "GET 'create'" do
     pending "twitter認証をMockしなきゃならんね"
+  end
+
+  describe "DELETE 'destroy'" do
+    it "destroys session by key ':account_id' " do
+      delete :destroy, {}, logged_in_session
+      expect( session[:account_id] ).to be_nil
+    end
+
+    it "redirects to the root url" do
+      delete :destroy, {}, logged_in_session
+      expect( response ).to redirect_to :root
+    end
+
   end
 
 end
